7ec3db2371fd5eb7bb2471f10486b98d7d363063
r41ns4n <rain.san@gmx.de>
2017-09-14 13:19:55 +0200

    Add Methode init Components(), Add Buttons

--- src/com/sabel/Farbenspiel.java
+++ src/com/sabel/Farbenspiel.java
@@ -6,15 +6,27 @@ public class Farbenspiel extends JFrame{
 
     // Datafields
     private JPanel jPanel;
+    private JButton jButtonBlue;
     private JButton jButtonRed;
     private JButton jButtonYellow;
-    private JButton jButtonBlue;
 
     public Farbenspiel() {
         this.setTitle("Farbenspiel");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponents();
         this.setSize(300,300);
         this.setVisible(true);
     } // End Constructor Farbenspiel()
+
+    public void initComponents() {
+        jPanel = new JPanel();
+        jButtonBlue = new JButton("Blue");
+        this.jPanel.add(jButtonBlue);
+        jButtonRed = new JButton("Red");
+        this.jPanel.add(jButtonRed);
+        jButtonYellow = new JButton("Yellow");
+        this.jPanel.add(jButtonYellow);
+        this.add(jPanel);
+    } // End Methode initComponents
 
 } // End Class Farbenspiel
