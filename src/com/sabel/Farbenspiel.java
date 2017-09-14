b0d9903d2981ea8153ca2fa50c72624dd5233c61
r41ns4n <rain.san@gmx.de>
2017-09-14 15:04:28 +0200

    Implemets ActinListener, Add Methode actionPerformed (ActionEvent actionEvent)

--- src/com/sabel/Farbenspiel.java
+++ src/com/sabel/Farbenspiel.java
@@ -1,8 +1,11 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Farbenspiel extends JFrame{
+public class Farbenspiel extends JFrame implements ActionListener{
 
     // Datafields
     private JPanel jPanel;
@@ -21,12 +24,28 @@ public class Farbenspiel extends JFrame{
     public void initComponents() {
         jPanel = new JPanel();
         jButtonBlue = new JButton("Blue");
+        jButtonBlue.addActionListener(this);
         this.jPanel.add(jButtonBlue);
         jButtonRed = new JButton("Red");
+        jButtonRed.addActionListener(this);
         this.jPanel.add(jButtonRed);
         jButtonYellow = new JButton("Yellow");
+        jButtonYellow.addActionListener(this);
         this.jPanel.add(jButtonYellow);
         this.add(jPanel);
     } // End Methode initComponents
+
+    public void actionPerformed (ActionEvent actionEvent) {
+        if(actionEvent.getSource() == this.jButtonBlue) {
+            jPanel.setBackground(Color.blue);
+        } // End if Blue
+        else if (actionEvent.getSource() == this.jButtonRed) {
+            jPanel.setBackground(Color.red);
+        } // End if Red
+        else if (actionEvent.getSource() == this.jButtonYellow) {
+            jPanel.setBackground(Color.yellow);
+        } // End if Yellow
+
+    } // End Methode actionPerformed
 
 } // End Class Farbenspiel
